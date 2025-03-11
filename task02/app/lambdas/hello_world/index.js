320c1db65dc1d4afab2d7045dafbdced337d0f02
Vaibhav Chauhan <vaibhav_chauhan@epam.com>
2025-03-11 10:14:11 +0530

    Done with task

---
+++ task02/app/lambdas/hello_world/index.js
@@ -0,0 +1,20 @@
+exports.handler = async (event) => {
+    const path = event.rawPath || event.path;
+    const method = event.requestContext?.http?.method || event.httpMethod;
+    if (path === "/hello" && method === "GET") {
+      return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ statusCode: 200, message: "Hello from Lambda" }),
+      };
+    }
+    return {
+      statusCode: 400,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        statusCode: 400,
+        message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
+      }),
+    };
+  };
+   
